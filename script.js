9b86200ac6d221d04728e227b7f562bb4a31a089
Adrián Alvarez <91921528+Cordobot@users.noreply.github.com>
2026-05-16 09:45:26 -0500

    Favicon
    Se modifica favicon se agrega un SVG se agrega texto en la intro de android developer

--- script.js
+++ script.js
@@ -157,10 +157,6 @@ function startIntro() {
 
     // 3. Reveal Subtitle
     setTimeout(() => {
-        if (window.innerWidth < 768) {
-            sub.textContent = "EXPERIENCIA";
-            sub.style.letterSpacing = "0.4em";
-        }
         sub.classList.add('active');
     }, 1800);
 
